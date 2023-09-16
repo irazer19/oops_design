c822d4d36c97fdbf9242432bd6294cbe428212b7
saurav_gupta <saurav.gupta@redhuntlabs.com>
2023-09-16 17:50:01 +0530

    Parking lot

---
+++ parking_lot/display_board.py
@@ -0,0 +1,19 @@
+class DisplayBoard:
+    def __init__(self, id, handicapped_spot, compact_spot, large_spot, motorbike_spot):
+        self.__id = id
+        self.__handicapped_spot = handicapped_spot
+        self.__compact_spot = compact_spot
+        self.__large_spot = large_spot
+        self.__motorbike_spot = motorbike_spot
+
+    def show_free_slot(self):
+        pass
+
+
+class ParkingRate:
+    def __init__(self, hours, rate):
+        self.__hours = hours
+        self.__rate = rate
+
+    def calculate(self):
+        pass
