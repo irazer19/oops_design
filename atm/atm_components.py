0c0a3999661c2500f30cee74eaf658733b8ee0d5
saurav_gupta <saurav.gupta@redhuntlabs.com>
2023-10-18 19:35:36 +0530

    Atm

---
+++ atm/atm_components.py
@@ -0,0 +1,23 @@
+class CardReader:
+    def readCard(self):
+        pass
+
+
+class CashDispenser:
+    def dispenseCash(self):
+        pass
+
+
+class Keypad:
+    def getInput(self):
+        pass
+
+
+class Screen:
+    def showMessage(self):
+        pass
+
+
+class Printer:
+    def printReceipt(self):
+        pass
